04b0f28570b796e2f5c41c355d3fbcb29cc7c10e
Renan Xavier <renansxdev@gmail.com>
2022-07-02 18:03:41 -0300

    changes

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,18 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    const { admin, updated_at } = request.body;
+    try {
+      const user = this.turnUserAdminUseCase.execute({
+        user_id,
+        ...admin,
+        updated_at,
+      });
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
